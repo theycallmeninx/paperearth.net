3b13d4c9dfb5b7cc512d296e0f9dfa0ba1629542
Ubuntu <ubuntu@ip-172-31-27-51.us-west-2.compute.internal>
2016-10-04 01:50:52 +0000

    Adjusted settings for Amazon EC2 instance

--- django-tutorial/mysite/mysite/settings.py
+++ django-tutorial/mysite/mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '@j@sack4^1^1kb+jy@*xcxgu9v2=_%%xmwcgz*#0n(7urr8lym'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0',
+                 'paperearth.net' 
+                ]
 
 
 # Application definition
