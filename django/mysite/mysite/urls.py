137fe3a43be9742488bdf2a2d210b5fb9c374cc9
Alex Boyle <alex.j.boyle@gmail.com>
2016-11-12 08:10:57 -0800

    NOW adding favicon.png

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -4,10 +4,7 @@ from django.contrib import admin
 
 from . import views
 
-favicon_view = RedirectView.as_view(url='/images/favicon.ico', permanent=True)
-
 urlpatterns = [
-    url(r'^favicon\.ico$', favicon_view),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^email/$', views.email, name='email'),
 	url(r'^polls/', include('polls.urls'), name='polls'),
