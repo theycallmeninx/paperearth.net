b320d6dc20300d502fa1ab4fdac8c20ac81861e1
Alex Boyle <alex.j.boyle@gmail.com>
2016-11-12 07:59:52 -0800

    Adding favicon.ico

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
 from django.contrib import admin
 
 from . import views
 
+favicon_view = RedirectView.as_view(url='/images/favicon.ico', permanent=True)
+
 urlpatterns = [
+    url(r'^favicon\.ico$', favicon_view),
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^email/$', views.email, name='email'),
 	url(r'^polls/', include('polls.urls'), name='polls'),
