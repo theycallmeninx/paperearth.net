d5534839643798cd762857bc9865b8e61540b717
Alex Boyle <alex.j.boyle@gmail.com>
2016-11-21 20:26:04 -0800

    Fixing unknown bug in initmaps

--- django/mysite/maps/static/maps/script.js
+++ django/mysite/maps/static/maps/script.js
@@ -31,32 +31,31 @@ function initMap() {
   
   map.addListener('dragend', function() {
     $.ajax({
-            type:"POST",
-            url:"./test/",
-            data: map.getBounds().toJSON(),
-            success: function(response){
-                for (var block in response) {
-                  var blockcoords = [];
-                  var street = response[block];
-                  for (var coords in street) {
-                    blockcoords.push({ 
-                      lat: parseFloat(street[coords]['lat']),
-                      lng: parseFloat(street[coords]['lng'])
-                    });
-                  }
-                  var zone = new google.maps.Polygon({
-                    paths: blockcoords,
-                    strokeColor: '#FF0000',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 2,
-                    fillColor: '#FF0000',
-                    fillOpacity: 0.25
-                  });
-                  zone.setMap(map);
-                }
-              }
-
+      type:"POST",
+      url:"./test/",
+      data: map.getBounds().toJSON(),
+      success: function(response){
+        for (var block in response) {
+          var blockcoords = [];
+          var street = response[block];
+          for (var coords in street) {
+            blockcoords.push({ 
+              lat: parseFloat(street[coords]['lat']),
+              lng: parseFloat(street[coords]['lng'])
+            }); 
+          }
+          var zone = new google.maps.Polygon({
+            paths: blockcoords,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.25
           });
+          zone.setMap(map);
+        }
+      }
+    });
   });
 
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(coordsDiv);
