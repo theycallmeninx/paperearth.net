3f638492027dd90943973fefe79ea70af6a8d6a2
Alex Boyle <Alex.J.Boyle@gmail.com>
2017-01-23 15:55:58 -0800

    Naming SECRET_KEY var correctly. I'm an idiot.

--- django/mysite/mysite/base_settings.py
+++ django/mysite/mysite/base_settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 if 'SECRETKEY' in os.environ:
-    SECRETKEY = os.environ['SECRETKEY']
+    SECRET_KEY = os.environ['SECRETKEY']
 else:
     secretkey = os.path.expanduser("~/gitrepos/webdocs/paperearth/secretkey.txt")
     with open(secretkey) as f:
