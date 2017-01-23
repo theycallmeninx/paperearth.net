db7278f5de9f68ce6b366a85cdcb28c4e7c21428
Alex Boyle <Alex.J.Boyle@gmail.com>
2017-01-23 11:59:44 -0800

    appending script path to sys path for wsgi.py

--- django/mysite/mysite/wsgi.py
+++ django/mysite/mysite/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.realpath(__file__))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
