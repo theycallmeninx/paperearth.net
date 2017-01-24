1f7ba8bf1dacfeac61c8090b8169a602665fb2bb
Alex Boyle <Alex.J.Boyle@gmail.com>
2017-01-24 10:56:31 -0800

    Adding logging info to settings.py

--- django/mysite/mysite/base_settings.py
+++ django/mysite/mysite/base_settings.py
@@ -157,3 +157,36 @@ STATICFILES_DIRS = (
                     os.path.join(BASE_DIR, 'maps', 'static'),
                     os.path.join(BASE_DIR, 'uploads'),
                     )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+        'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'paperearth.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+         },
+        'customer': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+         },
+    }
+}
